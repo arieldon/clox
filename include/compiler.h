2b3b233e63995b58f8414536d12c78f0b66706d3
Ariel Don <ariel@arieldon.com>
2022-04-04 20:22:22 -0500

    Parse expressions

--- include/compiler.h
+++ include/compiler.h
@@ -1,6 +1,39 @@
 #ifndef COMPILER_H
 #define COMPILER_H
 
-void compile(const char *source);
+#include "chunk.h"
+#include "common.h"
+#include "scanner.h"
+
+typedef struct {
+    Token current;
+    Token previous;
+    bool had_error;
+    bool panic_mode;
+} Parser;
+
+typedef enum {
+    PREC_NONE,
+    PREC_ASSIGNMENT,
+    PREC_OR,
+    PREC_AND,
+    PREC_EQUALITY,
+    PREC_COMPARISON,
+    PREC_TERM,
+    PREC_FACTOR,
+    PREC_UNARY,
+    PREC_CALL,
+    PREC_PRIMARY,
+} Precedence;
+
+typedef void (*ParseFn)();
+
+typedef struct {
+    ParseFn prefix;
+    ParseFn infix;
+    Precedence precedence;
+} ParseRule;
+
+bool compile(const char *source, Chunk *chunk);
 
 #endif
