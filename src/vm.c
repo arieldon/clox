0ace85987c1505f7cf2f55eff1f23487cd921d8c
Ariel Don <ariel@arieldon.com>
2022-04-05 16:40:01 -0500

    Add nil and boolean types

--- src/vm.c
+++ src/vm.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <stdarg.h>
 #include <stdio.h>
 
 #include "common.h"
@@ -15,6 +16,21 @@ static void
 resetStack(void)
 {
     vm.stack_top = vm.stack;
+}
+
+static void
+runtimeError(const char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    vfprintf(stderr, format, args);
+    va_end(args);
+    fputs("\n", stderr);
+
+    size_t instruction = vm.ip - vm.chunk->code - 1;
+    int line = vm.chunk->lines[instruction];
+    fprintf(stderr, "[line %d] in script\n", line);
+    resetStack();
 }
 
 void
@@ -43,16 +59,27 @@ pop(void)
     return *vm.stack_top;
 }
 
+static Value
+peek(int distance)
+{
+    assert(vm.stack_top - 1 - distance > vm.stack);
+    return vm.stack_top[-1 - distance];
+}
+
 static InterpretResult
 run(void)
 {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
-#define BINARY_OP(op) \
+#define BINARY_OP(value_type, op) \
     do { \
-        double b = pop(); \
-        double a = pop(); \
-        push(a op b); \
+        if (!IS_NUMBER(peek(0)) || !IS_NUMBER(peek(1))) { \
+            runtimeError("operands must be numbers"); \
+            return INTERPRET_RUNTIME_ERROR; \
+        } \
+        double b = AS_NUMBER(pop()); \
+        double a = AS_NUMBER(pop()); \
+        push(value_type(a op b)); \
     } while (false)
 
     /// XXX Upwards of 90% of execution time spent here according to Nystrom.
@@ -77,11 +104,20 @@ run(void)
                 push(constant);
                 break;
             }
-            case OP_ADD:      BINARY_OP(+); break;
-            case OP_SUBTRACT: BINARY_OP(-); break;
-            case OP_MULTIPLY: BINARY_OP(*); break;
-            case OP_DIVIDE:   BINARY_OP(/); break;
-            case OP_NEGATE:   push(-pop()); break;
+            case OP_NIL:      push(NIL_VAL); break;
+            case OP_TRUE:     push(BOOL_VAL(true)); break;
+            case OP_FALSE:    push(BOOL_VAL(false)); break;
+            case OP_ADD:      BINARY_OP(NUMBER_VAL, +); break;
+            case OP_SUBTRACT: BINARY_OP(NUMBER_VAL, -); break;
+            case OP_MULTIPLY: BINARY_OP(NUMBER_VAL, *); break;
+            case OP_DIVIDE:   BINARY_OP(NUMBER_VAL, /); break;
+            case OP_NEGATE:
+                if (!IS_NUMBER(peek(0))) {
+                    runtimeError("operand must be a number");
+                    return INTERPRET_RUNTIME_ERROR;
+                }
+                push(NUMBER_VAL(-AS_NUMBER(pop())));
+                break;
             case OP_RETURN: {
                 printValue(pop());
                 printf("\n");
